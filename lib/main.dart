478b4ef69791c7cf182888bdb309525c3e9db9fb
gamgoon <gamgoon@gmail.com>
2019-03-16 10:54:51 +0900

    add icons to the list

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
 
 class RandomWordState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
@@ -44,10 +45,15 @@ class RandomWordState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style:_biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite :Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
