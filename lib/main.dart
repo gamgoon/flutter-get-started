b762350f6556c9fa49a5104dcb84ebaebf99f5a7
gamgoon <gamgoon@gmail.com>
2019-03-16 11:38:07 +0900

    Navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,9 @@ class RandomWordState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[  // title 다음에 보여질 위젯. 앱바에 타이틀과 어떤 액션을 하는 위젯들이 올수있다는거군.
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),  // 클릭하면 _pushSaved, 그런데 여기서 push는 Navigator의 stack 에 push 한다는 의미군.
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -54,6 +57,46 @@ class RandomWordState extends State<RandomWords> {
       trailing: new Icon(
         alreadySaved ? Icons.favorite :Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void> (
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map( // 저장된 WordPair를 ListTile 로 
+            (WordPair pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+          .toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
       ),
     );
   }
